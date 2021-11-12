1824a769bf43b0203b060ae7762697f507bb5529
serg155alternate <serg1557733@i.ua>
2021-11-12 16:44:28 +0200

    trying to use other ways with API

--- HW3/HWBlueBelt/script.js
+++ HW3/HWBlueBelt/script.js
@@ -2,53 +2,81 @@
 Сделать задания обмена валют используя ассоциативный массив (объект) подобной структуры.
  Добавьте дополнительные поля при надобности. Для обращения к нужному полю используйте []. */
 
-/*  let currency = prompt('enter currency');
+            //let currency = prompt('enter currency');
 
- let currencyRate = {
-     usdB : 26.5,
-     usdS : 26.3,
-     eurB : 30.55,
-     eurS : 30.3,
-     rubB : 0.30,
-     rubS : 0.36
- }
+            /* let currencyRate = {
+                usdB : 26.5,
+                usdS : 26.3,
+                eurB : 30.55,
+                eurS : 30.3,
+                rubB : 0.30,
+                rubS : 0.36
+            }
 
-if (currency.toLowerCase() === 'usd' || currency.toLowerCase() === 'eur' || currency.toLowerCase() === 'rub'){
-    let sum = prompt('Enter curency summ');
-    let rate = confirm('You want to buy?');
-    rate ? alert(`You usd summ : ${sum * currencyRate[currency +'B']} uah`): alert(`You usd summ : ${sum*currencyRate[currency +'S']} uah`);
-} else {
-    alert('Enter correct currency name')
-} */
+            if (currency.toLowerCase() === 'usd' || currency.toLowerCase() === 'eur' || currency.toLowerCase() === 'rub'){
+            let sum = prompt('Enter curency summ');
+            let rate = confirm('You want to buy?');
+            rate ? alert(`You usd summ : ${sum * currencyRate[currency +'B']} uah`): alert(`You usd summ : ${sum*currencyRate[currency +'S']} uah`);
+            } else {
+            alert('Enter correct currency name')
+            }  */
+
+//second 
 
 
-fetch('https://open.er-api.com/v6/latest/USD').then(res => res.json())
+/* fetch('https://open.er-api.com/v6/latest/USD').then(res => res.json())
      .then(data => {
             console.log(data.rates.UAH)
-            if (currency.toLowerCase() === 'usd' || currency.toLowerCase() === 'uah'){
-                let sum = prompt('Enter curency summ');
-               alert(`You usd summ : ${sum * data.rates.uah} uah`);
-            } else {
-                alert('Enter correct currency name')
-            }
-        })
+            let sum = prompt('Enter USD summ');
+            alert(`You usd summ : ${sum * data.rates.UAH} uah`);
+        }); */
 
-
-
-
-let url = 'https://open.er-api.com/v6/latest/' + currency.toUpperCase();
-
-
-fetch(url).then(res => res.json())
+function shoeCurencys() {
+    fetch('https://open.er-api.com/v6/latest').then(res => res.json())
      .then(data => {
-           console.log(data.rates.uah);
-           if (currency.toLowerCase() === 'usd' || currency.toLowerCase() === 'uah'){
-               let sum = prompt('Enter curency summ');
-              alert(`You usd summ : ${sum * data.rates.uah} uah`);
-           } else {
-               alert('Enter correct currency name')
-           }
+            alert(`Valid curencyss:  ${Object.keys(data.rates)}`);
+            currency = prompt('enter currency for exchange (usd, eur, rub...etc)') ;
         })
+    
+}
+let currency;
+confirm('You want to seee all currencys')? shoeCurencys() : currency = prompt('enter currency for exchange (usd, eur, rub...etc)') ;
+
+
+function exchange() {
+    let url = 'https://open.er-api.com/v6/latest/' + currency.toUpperCase();
+if (currency.toLowerCase() === 'usd' || currency.toLowerCase() === 'eur' || currency.toLowerCase() === 'rub'){
+fetch(url)
+    .then(res => res.json())
+    .then(data => {console.log(data.rates.UAH)
+           
+            console.log(data);
+            
+                let sum =  prompt('enter amount');
+                
+               alert(`You usd summ : ${sum * data.rates.UAH} uah`);
+            
+           });
+}else if(currency){
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {console.log(data.rates.UAH)
+           
+            console.log(data);
+            
+                let sum =  prompt('enter amount');
+                
+               alert(`You usd summ : ${sum * data.rates.UAH} uah`);
+            
+           });
+} else {
+        alert('Enter correct currency name');
+}
+};
+
+
+
+       
 
 
 
