e4f89154906925816a9aa6c247b042e6eeaa8edc
serg155alternate <serg1557733@i.ua>
2021-11-12 19:05:34 +0200

    Home work 4 objects-in-json-es6

---
+++ HW4/script.js
@@ -0,0 +1,101 @@
+//Домашнее задание: Ассоциативные массивы
+
+//3 persons
+
+//Сделать три ассоциативных массива a, b, c, в каждом из которых должны быть поля name и surname.
+let a = {
+    name: "Serg",
+    surname: "Stepanov",
+}
+let b = {
+    name: "Semen",
+    surname: "Ivanovv",
+}
+let c = {
+    name: "Maxim",
+    surname: "Petrenko",
+  
+}
+
+//different fields
+
+a.age = 25;
+b.sex = 'men';
+c.fatherName = 'Viktorovich';
+
+console.log(a, b, c);
+
+//fields check
+
+//("fatherName" in c)? alert(alert(c["fatherName"])): alert('not found');
+
+/* function getOptionalKey(obj) {
+    for (let key in obj){
+        if (key !== 'surname' && key !== 'name' ) {
+            alert( `${key} : ${obj[key]} `);
+        }
+    } 
+}
+getOptionalKey(a);
+
+getOptionalKey(b);
+
+getOptionalKey(c); */
+
+//or
+
+
+/* function getKey(obj, keyToFind) {
+    for (let key in obj){
+        if (key === keyToFind) {
+            alert(obj[key]);
+        }
+    }
+}
+
+getKey(a, 'age');
+getKey(b, 'sex');
+getKey(c, 'fatherName'); */
+
+
+//array of persons
+
+let persons = [a, b, c, {name : 'oleg', surname: "Pavlov",}];
+
+console.log(persons);
+
+
+//loop of persons Сделайте цикл, который выводит весь массив persons в форме объектов console.log(persons[i])
+
+
+for (let index in persons) {
+    console.log(persons[index]);
+}
+
+//loop of name and surname
+
+for (let i in persons) {
+    console.log(`${persons[i].name}  ${persons[i].surname} `);
+}
+
+//loop of loop of values
+
+
+for (let objct of persons) {
+     for(let key in objct){
+        console.log(`${objct[key]}`);
+    } 
+}
+
+//fullName  Сделайте цикл, которых добавляет поле fullName в каждый объект, 
+//содержащий ФИО. Учтите, что поле fathername не является обязательным.
+
+for (let objct of persons) {
+    if("fatherName" in objct){
+         objct.fullName = `${objct.name}  ${objct.surname} ${objct.fatherName}`;
+    } else {
+        objct.fullName = `${objct.name}  ${objct.surname}`;
+    }
+      
+}
+console.log(a, b, c);
