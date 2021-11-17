228ce1fb6ccf58294b48b077ef705283dea7f1c9
serg155alternate <serg1557733@i.ua>
2021-11-17 20:10:40 +0200

    part 2  HW5

--- HW5/script.js
+++ HW5/script.js
@@ -123,7 +123,7 @@ console.log(array); */
 
 //array fill nopush
 
-let array1 = [];
+/* let array1 = [];
 let elem1 = prompt('Enter element');
 for (let i = 0; elem1; i++){
     let elem1 = prompt('Enter element');
@@ -133,4 +133,80 @@ for (let i = 0; elem1; i++){
         console.log('enter element');
     }
 }
-console.log(array1);
+console.log(array1); */
+
+//infinite probability
+/* Создайте бесконечный цикл, который прерывается с помощью конструкции break,
+ когда Math.random() > 0.9. Код должен подсчитывать количество итераций и вывести это число с помощью alert. */
+
+/* for (let i = 0; i >= 0; i ++ ){
+    if (Math.random() > 0.9 ){
+        alert(`${i}`);
+        break;
+    } 
+    else {
+        console.log(i);
+    }
+} */
+//empty loop
+//Сделайте цикл с prompt, который прерывается по нажатию OK и продолжается по нажатию "Отмена" c пустым телом цикла.
+/* let answ;
+do {
+    answ = prompt('','1');
+    console.log(!answ);
+} while(!answ); */
+
+//progression sum Подсчитать сумму арифметической прогрессии от 1 до N c шагом 3 (1,4,7....) используя цикл for.
+n = 25;
+let sum = 0;
+for (let i = 0; i < n; i++ ){
+    sum += i; 
+}
+console.log(sum);
+
+//chess one line
+//Сформировать строку " # # # # # " с помощью цикла for. Длина строки может быть четной и нечетной, и указывается в одном месте в коде.
+
+function string(length) {
+    let str = '';
+    for(let i = 0; i < length; i++)
+    str +=' #'
+    document.write(str)
+}
+string(30);
+
+//Сформировать строку c помощью вложенных циклов. Для перевода строки используйте \n.
+let strNum = '';
+for (let i =0; i < 10; i++){
+    strNum +='\n <br>';
+    
+    for (let k =0; k < 10; k++){
+        strNum += `${k}`;
+    }
+    
+}
+
+document.write(strNum);
+console.log(strNum);
+//chess
+
+function chessDesc(width, height) {
+    let desk = '';
+    for (let i = 0; i < width; i++){
+        desk +='<br>';
+        for (let k = 0; k < height; k++){
+            if(i%2){
+              desk += `. #`;  
+            } else {
+            desk += `# .`;  
+            }  
+        } 
+    } 
+    document.write(desk);
+    
+}
+chessDesc(15, 30);
+
+//cubes
+
+
