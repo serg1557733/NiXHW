d3d6ef6a5832d487146adaa8edf003fa96d300c6
serg155alternate <serg1557733@i.ua>
2021-11-18 19:53:06 +0200

    HW

--- HW7/script.js
+++ HW7/script.js
@@ -37,10 +37,10 @@ for (let i = 0; i < 10; i++){
 
 let td = document.querySelectorAll('td');
 
-td.forEach((i) => {
+/* td.forEach((i) => {
     i.onmouseover = () => i.style.backgroundColor = 'yellow';
     i.onmouseout = () => i.style.backgroundColor = '';
-}) 
+})  */
 
 /* 
 Подсветить строку и столбец,
@@ -53,11 +53,17 @@ td.forEach((i) => {
 
 
  td.forEach((item, i) => {
-    item.onmouseover = () => {
+    item.onmouseover = (e) => {
+        
        item.style.backgroundColor = 'yellow'; 
        item.parentNode.style.backgroundColor = 'green';
-       console.log(item.cellIndex);
-       console.log(i);
+      
+/*       if (td[i].cellIndex === e.target.cellIndex){
+        for (let i=0; i <0; i++ ){};
+           console.log(td[i])
+        
+          
+      }; */
        
     } 
 
@@ -65,4 +71,68 @@ td.forEach((i) => {
         item.style.backgroundColor = '';
         item.parentNode.style.backgroundColor = '';
     }
+})
+
+//Calc + Calc Live 
+
+
+let fuel = document.querySelector('[name = "liters"]'),
+    way = document.querySelector('[name = "km"]'),
+    checkLiter = document.querySelector('#liter'),
+    checkUah = document.querySelector('#uah'),
+    priceInput = document.querySelector('.price__item'),
+    price = document.querySelector('[name = "price"]'),
+    reset = document.querySelector('.btn'),
+    outer = document.querySelector('.outer');
+
+
+function calcMyConsumption(fuelLiters, kmWay, pricePerLiter) {
+    if (fuelLiters && kmWay && pricePerLiter) {
+        pricePerLiter = price.value;
+        const result = (fuelLiters / kmWay) * 100 * pricePerLiter;
+        outer.textContent = `Ваш затраты на 100км пути ${result.toFixed(2)} грн`;
+        reset.style.display = 'block';
+
+    } else {
+        const result = (fuelLiters / kmWay) * 100;
+        outer.textContent = `Ваш расход на 100км пути ${result.toFixed(2)} литров`;
+        reset.style.display = 'block';
+
+    }
+}
+
+checkLiter.addEventListener('change', () => {
+    if (checkLiter.checked && fuel.value && way.value) {
+        priceInput.style.display = 'none';
+        calcMyConsumption(fuel.value, way.value);
+       
+    } else {
+        outer.textContent = `Заполните все формы`;
+
+    }
+})
+
+
+checkUah.addEventListener('change', () => {
+    if (checkUah.checked && fuel.value && way.value ) {
+        priceInput.style.display = 'block';
+        price.value = '';
+        outer.textContent = `Внесите цену`;
+        price.addEventListener('input', () => calcMyConsumption(fuel.value, way.value, price));
+    
+    } else {
+        outer.textContent = `Заполните данные формы`;
+
+    }
+})
+
+reset.addEventListener('click', () => {
+    fuel.value = ''; 
+    way.value = '';
+    price.value = '';
+    outer.textContent = '';
+    priceInput.style.display = 'none';
+    reset.style.display = 'none';
+
+
 })
