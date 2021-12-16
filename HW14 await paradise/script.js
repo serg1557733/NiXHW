c59c0aa57e43c70e6fffa83a6ffc2fabee4bce0e
serg155alternate <serg1557733@ukr.net>
2021-12-16 19:06:50 +0200

    Async and Promise CHAT

--- HW14 await paradise/script.js
+++ HW14 await paradise/script.js
@@ -66,7 +66,6 @@ function sendMessage(nick, message) {
     
 }
 
-
 let regYoutube = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/; //regExp for YouTube video
 
 function readMessage(msg) {
